7ec210d82124a0feaa8a6b064e5bde40cb549d68
msleis <56922670+msleis@users.noreply.github.com>
2019-10-24 11:42:33 +0200

    Add files via upload

---
+++ anguLearn/controllers/MainController.js
@@ -0,0 +1,40 @@
+'use strict'
+
+app.controller('greetController', ['$scope', function($scope){
+    $scope.greet = 'Aplikace pro výpočet vašeho Body Mass Indexu (BMI).';
+    $scope.calculation = 2 + 5;
+
+    $scope.bmi = function() {
+        const NASOBEK = 100;
+
+                var vaha = prompt('Jaká je vaše váha v kilogramech?', []);
+
+                var vyska = prompt('Jaká je vaše výška v centimetrech?', []);
+
+                var desetinaVysky = vyska / NASOBEK;
+                var vysledekNasobkuVysky = desetinaVysky * desetinaVysky;
+                var vypocetBmi = vaha / vysledekNasobkuVysky;
+
+                var zkracene = vypocetBmi;
+                // BMI tabulka: 18.5 podvýživa, 18.5-24.9 Normál, 25-29.9 Nadváha, 30.0+ Obézní
+
+                alert("Vaše BMI hodnota je: " + vypocetBmi.toFixed(2));
+
+                if (zkracene < 18.5) {
+                    alert("Jste podvyživený/á");
+                }
+                if (zkracene >= 18.5 && zkracene <= 24.9) {
+                    alert("Jste v normálu");
+                }
+                if (zkracene >= 25 && zkracene <= 29.9) {
+                    alert("Máte nadváhu");
+                }
+                if (zkracene > 30) {
+                    alert("Jste v obézní");
+                }
+
+
+    }
+
+}])
+
